f48cfe7d1b927c4153f1f6a3bd6963ce43838230
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-08-01 13:43:39 -0300

    Add files via upload

---
+++ coelho para aluno/sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+
+
+
+
+
+
+function setup() 
+{
+  createCanvas(600,700);
+  frameRate(80);
+
+  
+
+
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  background("darkblue");
+
+
+ 
+
+}
+
